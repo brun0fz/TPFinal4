205f92086cbbf5ce83963f19a36f053eb1efe7ce
Belen Robledo <beeelrobledo@gmail.com>
2022-09-30 14:32:10 -0300

    registro.php

--- TPFinal/Views/home.php
+++ TPFinal/Views/home.php
@@ -1,18 +1,21 @@
-<?php 
- include('header.php');
+<?php
+include('header.php');
 ?>
 
-<div class="login">
-    <form action="">
-        <input type="text" name="email" placeholder="Email" required>
-        <input type="password" name="password" placeholder="Password" required><br>
-        <button type="submit">Login</button>
-    </form>
-    <form action="">
-        <button type="submit">Register</button>
-    </form>
+<div class="home">
+    <div class="login">
+        <p>Iniciar Sesion</p>
+        <form action="">
+            <input type="text" name="email" placeholder="Email" required><br>
+            <input type="password" name="password" placeholder="Password" required><br>
+            <button type="submit">Login</button>
+        </form>
+        <form action="">
+            <button type="submit">Register</button>
+        </form>
+    </div>
 </div>
 
-<?php 
-  include('footer.php');
+<?php
+include('footer.php');
 ?>
