ea7bb9bb1a76528b0f895a693dba32ad4db8b312
Belen Robledo <beeelrobledo@gmail.com>
2022-10-11 04:06:38 -0300

    home changes

--- TPFinal/Views/duenioHome.php
+++ TPFinal/Views/duenioHome.php
@@ -8,30 +8,24 @@ include('navBar.php');
     <h2 class="pb-2 border-bottom">Bienvenido/a <?php echo $_SESSION["loggedUser"]->getNombre() ?> :)</h2>
     <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
         <div class="feature col">
-            <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                <svg class="bi" width="1em" height="1em">
-                    <use xlink:href="#collection"></use>
-                </svg>
+            <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 p-2">
+                <img src="<?php echo IMG_PATH . "reservasIcon.png" ?>" alt="" width="40" height="40">
             </div>
             <h3 class="fs-2">Reservas</h3>
             <p>Mira el listado de tus reservas confirmadas o en curso, visualiza el detalle de una reserva. </p>
             <a href="<?php echo FRONT_ROOT . "Duenio/ShowDuenioHome" ?>">Ver reservas (Coming soon...)</a><br>
         </div>
         <div class="feature col">
-            <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                <svg class="bi" width="1em" height="1em">
-                    <use xlink:href="#people-circle"></use>
-                </svg>
+            <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 p-2">
+                <img src="<?php echo IMG_PATH . "guardianesIcon.png" ?>" alt="" width="40" height="40">
             </div>
             <h3 class="fs-2">Guardianes</h3>
             <p>Visualiza el listado de guardianes, filtralos por ubicacion, reputacion y disponibilidad, busca un guardian para realizar una reserva.</p>
             <a href="<?php echo FRONT_ROOT . "Duenio/ShowListaGuardianesView" ?>">Ver listado de Guardianes</a>
         </div>
         <div class="feature col">
-            <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                <svg class="bi" width="1em" height="1em">
-                    <use xlink:href="#toggles2"></use>
-                </svg>
+            <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 p-2">
+                <img src="<?php echo IMG_PATH . "mascotasIcon.png" ?>" alt="" width="40" height="40">
             </div>
             <h3 class="fs-2">Mascotas</h3>
             <p>Ve tu listado de mascotas, añade nuevas mascotas, visualiza sus perfiles y modificalos.</p>
