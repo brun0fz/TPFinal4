28c23272be8933818d3efbfdb54738acefc224c1
brun0fz <brunofabrizio15@gmail.com>
2022-11-03 13:57:43 -0300

    update

--- TPFinal/Views/header.php
+++ TPFinal/Views/header.php
@@ -3,7 +3,7 @@
 
 <head>
     <title>Pet Hero</title>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link href="<?php echo CSS_PATH; ?>layout.css" rel="stylesheet" type="text/css" media="all">
 </head>
