ed3fc0cadd36890dcb32ea6590699902a24123e0
brun0fz <brunofabrizio15@gmail.com>
2022-11-03 13:36:27 -0300

    update

--- TPFinal/Views/show-cupon.php
+++ TPFinal/Views/show-cupon.php
@@ -15,36 +15,36 @@ include("nav-bar.php");
             <ul class="list-group mb-3" id="listCheckout">
                 <li class="list-group-item d-flex justify-content-between lh-sm">
                     <div>
-                    <small class="text-muted">Mascota</small>
+                        <small class="text-muted">Mascota</small>
                         <h6 class="my-0"><?php echo $mascota->getNombre() ?></h6>
                     </div>
                 </li>
                 <li class="list-group-item d-flex justify-content-between lh-sm">
                     <div>
-                    <small class="text-muted">Guardian</small>
+                        <small class="text-muted">Guardian</small>
                         <h6 class="my-0"><?php echo $guardian->getNombre() . " " . $guardian->getApellido() ?></h6>
                     </div>
                 </li>
                 <li class="list-group-item d-flex justify-content-between lh-sm">
                     <div>
-                    <small class="text-muted">Fecha de Entrada</small>
+                        <small class="text-muted">Fecha de Entrada</small>
                         <h6 class="my-0"><?php echo $reserva->getFechaInicio() ?></h6>
                     </div>
                 </li>
                 <li class="list-group-item d-flex justify-content-between lh-sm">
                     <div>
-                    <small class="text-muted">Fecha de Salida</small>
+                        <small class="text-muted">Fecha de Salida</small>
                         <h6 class="my-0"><?php echo $reserva->getFechaFin() ?></h6>
                     </div>
                 </li>
                 <li class="list-group-item d-flex justify-content-between lh-sm">
                     <div>
-                    <small class="text-muted">Precio Total de la Reserva</small>
+                        <small class="text-muted">Precio Total de la Reserva</small>
                         <h6 class="my-0">$<?php echo $reserva->getPrecioTotal() ?></h6>
                     </div>
                 </li>
                 <li class="list-group-item d-flex justify-content-between lh-sm">
-                    <h6 class="my-2 text-primary">Total Cupón de Pago</h6>
+                    <h6 class="my-2 text-primary">Total Cupón de Pago (50%)</h6>
                     <span class="my-2 text-primary">$<?php echo $cupon->getTotal() ?></span>
                 </li>
             </ul>
@@ -73,15 +73,15 @@ include("nav-bar.php");
                     </div>
                     <div class="col-md-12">
                         <label for="cc-numero" class="form-label">Credit card number</label>
-                        <input type="text" name="numero" class="form-control" id="cc-numero" placeholder="xxxx-xxxx-xxxx-xxxx" required>
+                        <input type="text" name="numero" class="form-control" id="cc-numero" pattern="[0-9]{16}" placeholder="xxxx-xxxx-xxxx-xxxx" required>
                     </div>
                     <div class="col-md-3">
                         <label for="cc-vencimiento" class="form-label">Vencimiento</label>
-                        <input type="text" name="vencimiento" class="form-control" id="cc-vencimiento" placeholder="mm/aa" required>
+                        <input type="text" name="vencimiento" class="form-control" id="cc-vencimiento" pattern="[0-9]{4}" placeholder="mm/aa" required>
                     </div>
                     <div class="col-md-3">
                         <label for="cc-cvv" class="form-label">CVV</label>
-                        <input type="text" name="cvv" class="form-control" id="cc-cvv" placeholder="xxx" required>
+                        <input type="text" name="cvv" class="form-control" id="cc-cvv" pattern="[0-9]{3}" placeholder="xxx" required>
                     </div>
                 </div>
 
