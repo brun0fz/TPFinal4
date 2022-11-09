007d5caea92d64ff1392528bc6ddd7e5a3b0f9c5
Belen Robledo <beeelrobledo@gmail.com>
2022-11-09 15:58:02 -0300

    update perfil mascota

--- TPFinal/Views/profile-mascota.php
+++ TPFinal/Views/profile-mascota.php
@@ -1,4 +1,9 @@
 <?php
+use DAO\DuenioDAO;
+use DAO\GuardianDAO;
+use DAO\MascotaDAO;
+use DAO\ReservaDAO;
+
 include('header.php');
 include('nav-bar.php');
 ?>
@@ -35,6 +40,59 @@ include('nav-bar.php');
             <?php } ?>
         </div>
     </div>
+
+    <hr class="my-5" />
+
+    <!-- Historial de Reservas (finalizadas)-------------------------------------------------------->
+    <div>
+        <h2 class="mb-4">Historial de Reservas</h2>
+        <?php if(empty($listaReservas)) { ?>
+            <p class="alert alert-danger m-4" style="width: fit-content">Todav&iacute;a no existen reservas finalizadas para mostrar.</p>
+        <?php } else { ?>
+        <div>
+        <?php foreach ($listaReservas as $reserva) {
+            $guardianDAO = new GuardianDAO();
+            $duenioDAO = new DuenioDAO();
+            $mascotaDAO = new MascotaDAO();
+            $reservaDAO = new ReservaDAO();
+
+            $guardian = $guardianDAO->BuscarId($reserva->getFkIdGuardian());
+            $duenio = $duenioDAO->BuscarId($reserva->getFkIdDuenio());
+            $mascota = $mascotaDAO->GetMascotaById($reserva->getFkIdMascota());
+            $review = $reservaDAO->GetReviewByIdReserva($reserva->getIdReserva());
+        ?>
+
+            <div class="card mb-3 shadow-sm">
+                <div class="row g-0">
+                    <div class="col-md-4 card-img-reserva position-relative">
+                        <img src="<?php echo IMG_PATH . $mascota->getRutaFoto() ?>" class="rounded-start img-reserva img-unselect position-absolute top-50 start-50 translate-middle">
+                    </div>
+                    <div class="col-md-8 p-1 position-relative">
+                        <div class="card-body">
+                            <h3 class="card-title"><b><?php echo "Reserva para " . $mascota->getNombre(); ?></b></h3>
+                            <h5><small class="card-text">desde el <b><?php echo $reserva->getFechaInicio() ?></b> hasta el <b><?php echo $reserva->getFechaFin(); ?></b></small></h5>
+                            <hr class="my-3" />
+                            <p class="card-text">Due&ntilde;o: <b><?php echo $duenio->getNombre() . " " . $duenio->getApellido(); ?></b></p>
+                            <p class="card-text">Guardian: <b><?php echo $guardian->getNombre() . " " . $guardian->getApellido(); ?></b></p>
+                            <p class="card-text">Direcci&oacute;n: <b><?php echo $guardian->getCalle() . " " . $guardian->getNumero() . " " . $guardian->getPiso() . " " . $guardian->getDepartamento() ?></b></p>
+                            <p class="card-text">Precio Total: <b>$<?php echo $reserva->getPrecioTotal(); ?></b></p>
+                            <?php if ($review) { ?>
+                                <div class="alert alert-dark" style="width: fit-content;">
+                                    <span class="card-text">Review: <b><i><?php echo $review->getComentario(); ?></i></b></span>
+                                    <b class="ms-1"><?php echo "- " . $review->getPuntaje(); ?></b>
+                                    <img src="<?php echo IMG_PATH . "pawFull.png"; ?>" class="my-auto mb-1 py-1 pe-1" width="26" height="26" alt="">
+                                </div>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        <?php } } ?>
+        </div>
+    </div>
+    <!---------------------------------------------------------------------------------------------->
+
 </div>
 
 
