9b2f3e911caaf75171d90d4cb550bfa7fbd256c1
Belen Robledo <beeelrobledo@gmail.com>
2022-11-16 15:35:04 -0300

    update error view

--- TPFinal/Views/error.php
+++ TPFinal/Views/error.php
@@ -5,12 +5,16 @@ include("nav-bar.php");
 
 <div class="container">
     <div class="row">
-        <div class="col-md-12 col-lg-6 border">
-            <h1 class="text-primary">ERROR</h1>
-            <h3>Se ha producido un error. Intente nuevamente.</h3>
+        <div class="col-md-12 col-lg-6 d-flex flex-column justify-content-center">
+            <h1 class="text-primary mx-auto my-5">Oops!</h1>
+            <h3 class="mx-auto">Se ha producido un error.</h3>
+            <h3 class="mx-auto">Intente nuevamente.</h3>
+            <a href="<?php echo FRONT_ROOT . "Home/Index"; ?>" class="mx-auto my-5"><button class="btn btn-primary">Volver al Inicio</button></a>
         </div>
-        <div class="col-md-12 col-lg-6 border">
-            
+        <div class="col-md-12 col-lg-6">
+            <div class="d-flex justify-content-center">
+                <img src="<?php echo ASSETS_PATH . "error.png" ?>" alt="error" class="img-unselect" width="60%" height="auto">
+            </div>
         </div>
     </div>
 </div>
