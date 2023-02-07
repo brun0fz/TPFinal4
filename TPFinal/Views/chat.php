5a660920a4ad6fb236d937e7a714c78156ac7454
Belen Robledo <beeelrobledo@gmail.com>
2023-02-06 22:41:38 -0300

    styled chat

--- TPFinal/Views/chat.php
+++ TPFinal/Views/chat.php
@@ -1,45 +1,76 @@
 <?php
-
 include("header.php");
 include("nav-bar.php");
 ?>
 
 <div class="container">
-    <div class="col-4">
-        <?php foreach($userList as $user){ ?>
-            <li><a href="<?php echo FRONT_ROOT . "Chat/ShowChatView/" . $user->getId() ?>"><?php echo $user->getNombre() ?></a></li>
-        <?php } ?>
-    </div>
-    <div class="col-8">
-        <?php if (!$userList) { ?>
-            <p> No existen chats</p>
-        <?php } else { ?>
-            <h1><?php echo $user2->getNombre() . " " . $user2->getApellido() ?></h1>
-            <?php foreach($chatList as $chat){
-                if($chat->getIdEmisor() == $user1->getId()){ ?>
-                    <p class="text-end">
-                        <span class="text-end"><?php echo $chat->getMensaje(); ?></span>
-                        <span class="text-end text-muted"><?php echo date_format(date_create($chat->getFecha()), 'j/n/y H:i') . "hs"; ?></span>
-                    </p>
-                <?php } else{ ?>
-                    <p class="text-start">
-                        <span class="text-start"><?php echo $chat->getMensaje(); ?></span>
-                        <span class="text-start text-muted"><?php echo date_format(date_create($chat->getFecha()), 'j/n/y H:i') . "hs"; ?></span>
-                    </p>
-                <?php }
-            } ?>
-            <form action="<?php echo FRONT_ROOT ?>Chat/Add" method="Post">
-                <div class="form-floating">
-                    <textarea class="form-control" name="mensaje" placeholder="mensaje" id="floatingTextarea" style="height: 120px"></textarea>
-                    <label for="floatingTextarea">Mensaje</label>
+    <div class="row bg-white">
+        <div class="col-12 col-sm-4 border border-secundary border-end-0 p-0">
+            <ul class="list-group list-group-flush">
+            <?php foreach($userList as $user){ ?>
+                <a class="list-group-item list-group-item-action py-4 d-flex border-bottom <?php if($user2->getId() == $user->getId()){echo "active";} ?>" href="<?php echo FRONT_ROOT . "Chat/ShowChatView/" . $user->getId() ?>">
+                    <img src="<?php echo IMG_PATH . $user->getRutaFoto() ?>" alt="profilePic" width="42" height="42" class="rounded-circle img-unselect my-auto me-2">    
+                    <p class="my-auto"><?php echo $user->getNombre() . " " . $user->getApellido() ?></p>
+                </a>
+            <?php } ?>
+            </ul>
+        </div>
+        <div class="col-12 col-sm-8 chat d-flex flex-column border border-secundary">
+                <div class="d-flex">
+                    <img src="<?php echo IMG_PATH . $user2->getRutaFoto() ?>" alt="profilePic" width="56" height="56" class="rounded-circle img-unselect my-auto me-2">
+                    <h2 class="my-4"><?php echo $user2->getNombre() . " " . $user2->getApellido() ?></h2>
                 </div>
-                <input type="hidden" name="idEmisor" value="<?php echo $user1->getId() ?>">
-                <input type="hidden" name="idReceptor" value="<?php echo $user2->getId() ?>">
-                <input class="btn btn-lg btn-primary w-100" type="submit" value="Enviar">
-            </form>
-        <?php } ?>
+                <hr class="mt-1 mb-3" />
+                <div class="chat-msgs overflow-auto" id="chatMsgs">
+                    <?php if (!$chatList) { ?>
+                        <p class="d-flex justify-content-center text-muted mt-3">Todavia no han iniciado una conversación.</p>
+                    <?php } else { ?>
+                        <?php foreach($chatList as $chat){
+                            if($chat->getIdEmisor() == $user1->getId()){ ?>
+                                <p class="text-end text-break alert alert-primary ms-auto me-1 rounded-5 rounded-end text-dark p-2" style="width: fit-content">
+                                    <span><?php echo $chat->getMensaje(); ?></span>
+                                    <span class="text-muted align-bottom"><sub><?php echo date_format(date_create($chat->getFecha()), 'j/n/y H:i') . "hs"; ?></sub></span>
+                                </p>
+                            <?php } else{ ?>
+                                <p class="text-start text-break alert alert-secondary ms-1 rounded-5 rounded-start text-dark p-2" style="width: fit-content">
+                                    <span><?php echo $chat->getMensaje(); ?></span>
+                                    <span class="text-muted"><sub><?php echo date_format(date_create($chat->getFecha()), 'j/n/y H:i') . "hs"; ?></sub></span>
+                                </p>
+                            <?php }
+                        } } ?>
+                    <span id="scrollToBottom"></span>
+                </div>
+                <div class="my-2">
+                    <form action="<?php echo FRONT_ROOT ?>Chat/Add" method="Post" class="d-flex">
+                        <textarea class="form-control chat-textarea" name="mensaje" placeholder="Escribe un mensaje" id="message"></textarea>
+                        <input type="hidden" name="idEmisor" value="<?php echo $user1->getId() ?>">
+                        <input type="hidden" name="idReceptor" value="<?php echo $user2->getId() ?>">
+                        <button type="submit" class="btn btn-sm btn-primary rounded-circle ms-1 my-1" id="submitBtn">
+                            <img src="<?php echo ASSETS_PATH . "sendButton.png" ?>" >
+                        </button>
+                    </form>
+                </div>
+        </div>
     </div>
 </div>
+
+<script>
+    const msg = document.getElementById('message');
+    const submitBtn = document.getElementById('submitBtn');
+
+    msg.addEventListener('keydown', function (evt) {
+        const keyCode = evt.which || evt.keyCode;
+        if (keyCode === 13 && !evt.shiftKey) {
+            evt.preventDefault();
+            submitBtn.click();
+        }
+    });
+</script>
+
+<script>
+    const chatMsgs = document.getElementById('scrollToBottom');
+    chatMsgs.scrollIntoView({block: "end"});
+</script>
 
 <?php
 include("footer.php");
